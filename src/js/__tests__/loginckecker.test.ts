1d90e8a381a405f95baaed30d0d48200976d7f8d
Dmitry Pigrov <pigrov@gmail.com>
2022-08-22 21:23:58 +0300

    final

--- src/js/__tests__/loginckecker.test.ts
+++ src/js/__tests__/loginckecker.test.ts
@@ -1,22 +1,22 @@
-import Validator from '../components/app';
+import LoginValidator from '../components/app';
 
-describe('validateUsername from Validator class: ', () => {
+describe('validate login from LoginValidator class: ', () => {
   const cases = [
-    ['_username'],
-    ['username_'],
-    ['-username'],
-    ['username-'],
-    ['username1'],
-    ['1username'],
-    ['абв'],
-    ['user1234'],
+    ['_errorlogin'], 
+    ['errorlogin_'],
+    ['-errorlogin'],
+    ['errorlogin-'],
+    ['errorlogin1'],
+    ['1errorlogin'],
+    ['ОшибкаЛогина'],
+    ['login123'],
   ];
 
-  test.each(cases)('method validateUsername should throw Error', (item) => {
-    expect(() => Validator.check(item)).toThrow(Error);
+  test.each(cases)('method LoginValidator should throw Error', (item) => {
+    expect(() => LoginValidator.check(item)).toThrow(Error);
   });
 
-  test('valid user name', () => {
-    expect(Validator.check('user_123-super-char')).toBeTruthy();
+  test('valid login', () => {
+    expect(LoginValidator.check('user_123-super-char')).toBeTruthy();
   });
 });
